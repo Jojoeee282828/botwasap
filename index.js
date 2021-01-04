d7cc3523d6011748d4c8f254f16eeb0849d7856c
Jojoeee282828 <73501127+Jojoeee282828@users.noreply.github.com>
2021-01-05 06:42:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const  XBOT = 'AMPIBI'; // Nama Bot Whatsapp
-const instagram = 'https://instagram.com/affis_saputro123'; // Nama Instagramlu cok
+const  XBOT = 'NthnBOT; // Nama Bot Whatsapp
+const instagram = 'https://instagram.com/jonathanmakarios'; // Nama Instagramlu cok
 const nomer = 'https://Wa.me/+6282334297175'; // Nomor whatsapplu cok
-const aktif = 'Tergantung kuota'; // Kapan bot lu aktif
-const groupwa = 'comming soon'; // OFFICIAL GRUP LU 1
-const youtube = 'https://youtube.com/channel/UCYKxsg7iF9a9IZyXQRNsvqw'; 
+const aktif = 'Random'; // Kapan bot lu aktif
+const groupwa = '-'; //OFFICIAL GRUP LU 1
+const youtube = 'https://youtube.com/c/JnathanXyz; 
 const qrcode = require("qrcode-terminal");
 const moment = require("moment");
 const cheerio = require("cheerio");
